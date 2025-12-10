6aeda593b44d60e34e95cc315f171d98bab37470
DuongTrun <125685311+DuongTrun@users.noreply.github.com>
2025-12-10 18:12:51 +0700

    Update WebConfig.java

--- src/main/java/com/organic/organic_food/config/WebConfig.java
+++ src/main/java/com/organic/organic_food/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.organic.organic_food.config;
+package com.organic.organic_food.config; // <--- Giữ nguyên tên package của bạn nhé
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -6,11 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOriginPatterns("*") // Cho phép mọi domain truy cập
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Cho phép mọi thao tác
-				.allowedHeaders("*") // Cho phép mọi loại dữ liệu đầu vào
-				.allowCredentials(true); // Cho phép gửi cookie/xác thực (nếu cần)
-	}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // Cho phép mọi domain truy cập
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Cho phép mọi thao tác
+                .allowedHeaders("*") // Cho phép mọi loại dữ liệu đầu vào
+                .allowCredentials(true); // Cho phép gửi cookie/xác thực (nếu cần)
+    }
 }
